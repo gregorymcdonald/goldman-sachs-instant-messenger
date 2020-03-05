8f75d537093736f352f6a2a9338ccb17eea3c7f3
Gregory McDonald <greg.ry.mcd@gmail.com>
2020-03-05 00:05:37 -0600

    Implements Channel

--- src/main/java/Channel.java
+++ src/main/java/Channel.java
@@ -1,13 +1,27 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Channel implements IChannel {
-    public Channel() {}
+    private ArrayList<Message> messages;
+
+    /**
+     * Default Channel constructor.
+     * Initializes an empty channel with no messages.
+     */
+    public Channel() {
+        this.messages = new ArrayList<Message>();
+    }
 
     public List<Message> GetMessages() {
-        return null;
+        // Deepcopy the internal messages array to prevent mangling.
+        ArrayList<Message> clone = new ArrayList<Message>(messages.size());
+        for(Message m : this.messages) {
+            clone.add(new Message(m));
+        }
+        return clone;
     }
 
     public void SendMessage(Message message) {
-        return;
+        this.messages.add(new Message(message));
     }
 }
