2c5acd087ca62485f771994076e6d0745ecbaa59
Gregory McDonald <greg.ry.mcd@gmail.com>
2020-03-04 19:53:29 -0600

    Adds instructions to run backend.

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ The prompt for this project is
 > Create a WebSocket-based AIM chat messenger client using React for the front-end and a Vert.x Java back-end.
 > Upload the project into a GitHub repository [the one your currently viewing] and send it to Adam Fleming for code review.
 
+## How to run
+1. Execute `mvn clean package` in the project root directory (requires [Maven](https://maven.apache.org/)). This command will create a JAR file in the **target** directory that can be executed.
+2. Execute `java -jar target/goldman-sachs-instant-messenger-1.0-SNAPSHOT-fat.jar` to run the JAR file.
+
+
 ## Helpful Links
 [Single Page Application development with React and Vert.x](https://how-to.vertx.io/single-page-react-vertx-howto/)
-
+[My first Vert.x 3 Application](https://vertx.io/blog/my-first-vert-x-3-application/)
--- src/main/java/BackendVerticle.java
+++ src/main/java/BackendVerticle.java
@@ -1,5 +1,31 @@
-public class BackendVerticle {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+import java.io.IOException;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class BackendVerticle extends AbstractVerticle {
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		super.start(startFuture);
+		Router router = Router.router(vertx);
+	    Route messageRoute = router.get("/api/message");
+	    messageRoute.handler(rc -> {
+	      rc.response().end("Hello React from Vert.x!");
+	    });
+
+	    router.get().handler(StaticHandler.create());
+
+	    vertx.createHttpServer()
+	      .requestHandler(router)
+	      .listen(8080);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Vertx vertx = Vertx.vertx();
+    	vertx.deployVerticle(new BackendVerticle());
 	}
 }
