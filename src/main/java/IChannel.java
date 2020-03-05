2d70b4dae457a082c94e1be738044039cd5b17ac
Gregory McDonald <greg.ry.mcd@gmail.com>
2020-03-05 00:05:51 -0600

    Adds JavaDoc

--- src/main/java/IChannel.java
+++ src/main/java/IChannel.java
@@ -1,6 +1,20 @@
 import java.util.List;
 
+/**
+ * Interface describing a conversation between one or more participants, i.e. a channel.
+ *
+ * @author Gregory McDonald
+ * @since Mar 04, 2020
+ */
 interface IChannel {
+    /**
+     * @return a List of all message(s) sent to the channel in the order they were sent.
+     */
     public List<Message> GetMessages();
+
+    /**
+     * Send a message to the channel.
+     * @param message - Message to send.
+     */
     public void SendMessage(Message message);
 }
--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -2,11 +2,20 @@ public class Message {
 	public String creator;
     public String content;
 
+	/**
+	 * Default Message constructor.
+	 * @param creator - name of the user that created this message.
+	 * @param content - text of the message
+	 */
     public Message(String creator, String content) {
     	this.creator = creator;
     	this.content = content;
     }
 
+	/**
+	 * Copy constructor for Message objects.
+	 * @param message - Message to copy.
+	 */
     public Message(Message message) {
     	this(message.creator, message.content);
     }
