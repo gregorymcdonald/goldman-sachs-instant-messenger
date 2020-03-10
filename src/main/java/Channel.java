777385e6b0376fa3949a34f1c2348008295bf05b
Gregory McDonald <greg.ry.mcd@gmail.com>
2020-03-10 01:09:08 -0500

    Removes redundant Channel interface

--- src/main/java/Channel.java
+++ src/main/java/Channel.java
@@ -5,7 +5,13 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-public class Channel implements IChannel {
+/**
+ * Interface describing a conversation between one or more participants, i.e. a channel.
+ *
+ * @author Gregory McDonald
+ * @since Mar 10, 2020
+ */
+public class Channel {
     private String identifier;
     private Set<String> members;
     private List<Message> messages;
@@ -20,14 +26,25 @@ public class Channel implements IChannel {
         messages = new ArrayList<Message>();
     }
 
+    /**
+     * @return a string uniquely identifying the channel. This identifier will not change over the lifetime of the channel.
+     */
     public String GetIdentifier() {
         return identifier;
     }
 
+    /**
+     * @return a List of all user(s) that are members of the channel.
+     */
     public Collection<String> GetMembers() {
         return new HashSet(members);
     }
 
+    /**
+     * Add a member to the channel.
+     *
+     * @param user - the user to add to the channel
+     */
     public void AddMember(String user) {
         if (user == null || user.isEmpty()) {
             throw new IllegalArgumentException("user can not be null or empty");
@@ -35,10 +52,18 @@ public class Channel implements IChannel {
         members.add(user);
     }
 
+    /**
+     * Remove a member from the channel.
+     *
+     * @param user - the user to remove from the channel
+     */
     public void RemoveMember(String user) {
         members.remove(user);
     }
 
+    /**
+     * @return a List of all message(s) sent to the channel in the order they were sent.
+     */
     public List<Message> GetMessages() {
         // Deepcopy the internal messages array to prevent mangling.
         ArrayList<Message> clone = new ArrayList<Message>(messages.size());
@@ -48,6 +73,10 @@ public class Channel implements IChannel {
         return clone;
     }
 
+    /**
+     * Send a message to the channel.
+     * @param message - Message to send.
+     */
     public void SendMessage(Message message) {
         this.messages.add(new Message(message));
     }
