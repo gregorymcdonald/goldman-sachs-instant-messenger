19efa4f78df773623ff747d48f9511ccb06cfebf
Gregory McDonald <greg.ry.mcd@gmail.com>
2020-03-11 16:14:26 -0500

    Adds messages panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,31 @@ import { render } from 'react-dom';
 import ChannelPanel from './channel-panel/ChannelPanel';
 
 import './App.scss';
+import MessagesPanel from './messages-panel/MessagesPanel';
 
 class App extends React.Component {
     state = {
       username: "Gregory",
       channels: [],
+      selectedChannel: {
+          messages: [
+              {
+                  creator: "Gregory",
+                  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe"
+              },
+              {
+                creator: "Hannah",
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe"
+              }
+          ]
+      }
     };
   
     handleChange(event) {
       this.setState({username: event.target.value})
     }
   
-    componentDidMount() { // (5)
+    componentDidMount() {
       fetch("http://localhost:8080/api/channels?member=Gregory")
         .then(response => response.json())
         .then(json => this.setState({channels: json}));
@@ -25,6 +38,7 @@ class App extends React.Component {
       return (
         <div className="container">
             <ChannelPanel user={this.state.username} channels={this.state.channels} />
+            <MessagesPanel user={this.state.username} messages={this.state.selectedChannel.messages}/>
             <input className="username" type="text" value={this.state.username} onChange={this.handleChange.bind(this)}/>
         </div>
       );
---
+++ frontend/src/messages-panel/MessagesPanel.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import './MessagesPanel.scss';
+
+interface Message {
+  creator: string;
+  content: string;
+}
+
+interface Props {
+  user: string;
+  messages?: Message[];
+}
+
+export default class MessagesPanel extends React.Component<Props> {
+  state = {
+    textMessage: ''
+  };
+
+  private handleTextMessageChange(event) {
+    this.setState({textMessage: event.target.value})
+  }
+
+  render () {
+    const messages = this.props.messages.map(
+      (message, i) => {
+        const className: string = `message${message.creator === this.props.user ? ' you' : ''}`;
+        return (
+          <div className={className} key={i}>
+              <span>{message.content}</span>
+          </div>
+        );
+      }
+    );
+
+    return (
+      <div className="messages-panel">
+          <h1>Messages</h1>
+          <div className="message-scroll">
+            {messages}
+          </div>
+          <div className="compose-container">
+            <input className="compose-input" type="text" placeholder="Text message" value={this.state.textMessage} onChange={this.handleTextMessageChange.bind(this)}/>
+            <button>Send</button>
+          </div>
+      </div>
+    );
+  }
+}
