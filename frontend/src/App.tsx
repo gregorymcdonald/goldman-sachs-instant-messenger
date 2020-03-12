769bf5be22e07808e339c15ab54de1e7159619f3
Gregory McDonald <greg.ry.mcd@gmail.com>
2020-03-11 23:16:37 -0500

    Uses channel identifier instead of channel object

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,22 +10,22 @@ import { Channel } from './shared/Channel';
 interface State {
     username: string,
     channels: Channel[],
-    selectedChannel: Channel
+    selectedChannelIdentifier: string
 }
 
 class App extends React.Component {
     public state: State = {
       username: "Gregory",
       channels: [],
-      selectedChannel: null
+      selectedChannelIdentifier: null
     };
   
     handleChange(event) {
       this.setState({username: event.target.value})
     }
 
-    private onChannelClick(channel): void {
-        this.setState({selectedChannel: channel});
+    private onChannelClick(channel: Channel): void {
+        this.setState({selectedChannelIdentifier: channel.identifier});
     }
   
     componentDidMount() {
@@ -35,11 +35,12 @@ class App extends React.Component {
     }
   
     render () {
+      const selectedChannel: Channel = this.state.channels.find(v => v.identifier === this.state.selectedChannelIdentifier)
       return (
         <div className="container">
             <ChannelPanel user={this.state.username} channels={this.state.channels} onChannelClick={this.onChannelClick.bind(this)}/>
-            { this.state.selectedChannel
-                ? <MessagesPanel user={this.state.username} messages={this.state.selectedChannel.messages}/>
+            { selectedChannel
+                ? <MessagesPanel user={this.state.username} messages={selectedChannel.messages}/>
                 : null
             }
             <input className="username" type="text" value={this.state.username} onChange={this.handleChange.bind(this)}/>
